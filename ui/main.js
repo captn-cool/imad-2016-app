bc3c9b33f07ba1f282709e6ec1dcef5f93b3db4a
captn-cool <nikdude80@gmail.com>
2016-10-10 17:40:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,17 @@ console.log('Loaded!');
 var element=document.getElementById('maintxt');
 
 element.innerHTML='no Value..';
+
+var img=document.getElementById('madi');
+
+right=0;
+function moveright(){
+     right+=10;
+     img.style.marginleft=right+'px';
+    
+} 
+
+img.onclick=function(){
+    var interval=setInterval(moveright,50);
+    
+}
